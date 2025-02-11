e9f87402567b6dfe4cf89ab06e137ad9e1df0fe4
bebz0 <romanbizonua@gmail.com>
2025-02-11 10:11:11 +0200

    made some changes in main.py

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ from utilts import *
 print(func(12))
 print(func(14))
 print(func(17))
+from utilts import factorial
+
+
+n = int(input("enter num for !"))
+print(factorial(n))
+gitg
--- utilts.py
+++ utilts.py
@@ -16,11 +16,9 @@ def factorial(n):
     res = 1
     for i in range(n):
         res *= i+1
-
     return res
-
   
- def is_prime(n):
+def is_prime(n):
     if n < 2:
         return False
     for i in range(2, int(n**0.5) + 1):
